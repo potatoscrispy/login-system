c2004dcd655c404339aca4f34e141f39317807b6
potatoscrispy <a.elhawary1998199@gmail.com>
2024-06-14 02:08:03 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var loginBtn = document.getElementById("login");
+var signupBtn = document.getElementById("signup");
+var emailLogin = document.getElementById("emailLogin");
+var passwordLogin = document.getElementById("passwordLogin");
+var emailSignup = document.getElementById("emailSignup");
+var passwordSignup = document.getElementById("passwordSignup");
+
+loginBtn.addEventListener("click", function () {});
